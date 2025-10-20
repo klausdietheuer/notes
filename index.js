5a69a30ae8a8bb309505040cb5cc056009ac36c8
dietheuer <dietheuer@staatsanzeiger-eservices.de>
2025-10-20 14:14:46 +0200

    fix port for render

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
